fbade93b4b3735cbdbc6ad5c843a022682d0e483
WenJiang <WenJiangPR@gmail.com>
2017-03-14 19:53:51 +0800

    Renew

---
+++ CAD/main.c
@@ -0,0 +1,25 @@
+/*
+*    main.c
+*    C Project
+*	 Team production.
+*	 Team member: Jiang Wen, Huang Nianmei, Li Jianing.
+*    This file was created by Wen Jiang on 3/14/2017.
+*    Copyright (c) 2017 Wen Jiang. All rights reserved.
+*/
+#include"main.h"
+TimerEventCallback TimerEvent(int timerID);
+CharEventCallback CharEvent(char c);
+KeyboardEventCallback KeyboardEvent(int key, int event);
+MouseEventCallback MouseEvent(int x, int y, int button, int event);
+int Setup() {
+	initWindow("CAD", DEFAULT, DEFAULT, 1024, 768);
+	registerTimerEvent(TimerEvent);
+	registerKeyboardEvent(KeyboardEvent);
+	registerCharEvent(CharEvent);
+	registerMouseEvent(MouseEvent);
+	return 0;
+}
+TimerEventCallback TimerEvent(int timerID) { ; }
+CharEventCallback CharEvent(char c) { ; }
+KeyboardEventCallback KeyboardEvent(int key, int event) { ; }
+MouseEventCallback MouseEvent(int x, int y, int button, int event) { ; }
---
+++ CAD/main.h
@@ -0,0 +1,16 @@
+/*
+*    main.h
+*    C Project
+*	 Team production.
+*	 Team member: Jiang Wen, Huang Nianmei, Li jianing.
+*    This file was created by Wen Jiang on 3/14/2017.
+*    Copyright (c) 2017 Wen Jiang. All rights reserved.
+*/
+#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "acllib.h"
+
+#endif // !MAIN_H
+
