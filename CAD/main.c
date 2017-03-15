d95878812c61ae4e52f11477cb155b12ab75b75c
WenJiang <WenJiangPR@gmail.com>
2017-03-15 11:10:57 +0800

    typedef

--- CAD/main.c
+++ CAD/main.c
@@ -13,6 +13,7 @@ KeyboardEventCallback KeyboardEvent(int key, int event);
 MouseEventCallback MouseEvent(int x, int y, int button, int event);
 int Setup() {
 	initWindow("CAD", DEFAULT, DEFAULT, 1024, 768);
+	initConsole();
 	registerTimerEvent(TimerEvent);
 	registerKeyboardEvent(KeyboardEvent);
 	registerCharEvent(CharEvent);
@@ -22,4 +23,4 @@ int Setup() {
 TimerEventCallback TimerEvent(int timerID) { ; }
 CharEventCallback CharEvent(char c) { ; }
 KeyboardEventCallback KeyboardEvent(int key, int event) { ; }
-MouseEventCallback MouseEvent(int x, int y, int button, int event) { ; }
+MouseEventCallback MouseEvent(int x, int y, int button, int event) { printf("x=%d, y=%d\n", x, y); }
--- CAD/main.h
+++ CAD/main.h
@@ -11,6 +11,13 @@
 #define MAIN_H
 
 #include "acllib.h"
-
+#include<stdio.h>
+/*Use typedefine to set our own type. WJ*/
+typedef int int32_t;
+typedef unsigned int  uint32_t;
+typedef char char8_t;
+typedef unsigned char uchar8_t;
+typedef double double64_t;
+typedef unsigned double udouble64_t;
+//Belowes
 #endif // !MAIN_H
-
