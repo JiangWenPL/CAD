f79528f7d9ec10c58f0c3e2a1036da002b0eb72f
WenJiang <WenJiangPR@gmail.com>
2017-03-26 15:54:09 +0800

    Add many many input functions.

--- CAD/main.c
+++ CAD/main.c
@@ -14,6 +14,8 @@
 //}MsgQueue;
 
 int32_t m_lMode = 0;
+int32_t m_Text_Postion_x = 0;
+int32_t m_Text_Postion_y = 0;
 ACL_Color m_CurrentColor = BLACK;
 MsgQueue* p_MsgHead = NULL;
 MsgQueue CAD_Msg;
@@ -22,7 +24,22 @@ int32_t DrawRectangle(int x0, int y0, int size, int style);
 int32_t Erase(int x0, int y0, int size, int style);
 int32_t Craming(int x0, int y0, int size, int style);
 int32_t Words(int x0, int y0, int size, int style);
-int32_t StraightLine(int x0, int y0, int size, int style);
+//bellows are newly added function
+int32_t	Regular_Triangle(int x0, int y0, int size, int style) { ; }
+int32_t Right_Triangle(int x0, int y0, int size, int style) { ; }
+int32_t Five_Star(int x0, int y0, int size, int style) { ; }
+int32_t Up_Arrow(int x0, int y0, int size, int style) { ; }
+int32_t DrawSquare(int x0, int y0, int size, int style) { ; }
+int32_t DrawCurve(int x0, int y0, int size, int style) { ; }
+int32_t DrawCircle(int x0, int y0, int size, int style) { ; }
+int32_t Arc_Rectangle(int x0, int y0, int size, int style) { ; }
+int32_t DrawPentagon(int x0, int y0, int size, int style) { ; }
+int32_t DownArrow(int x0, int y0, int size, int style) { ; }
+int32_t Hexagon(int x0, int y0, int size, int style) { ; }
+int32_t Rhombus(int x0, int y0, int size, int style) { ; }
+int32_t Six_Star(int x0, int y0, int size, int style) { ; }
+int32_t ForeStar(int x0, int y0, int size, int style) { ; }
+//barcket the latetst comment
 MsgQueue* NewMsgQueue();
 MsgQueue* Push_Back_Msg(const MsgQueue* p_CurrentMsg);
 MsgQueue Pop_Msg();
@@ -43,55 +60,65 @@ int Setup() {
 }
 TimerEventCallback TimerEvent(int timerID) { ; }
 CharEventCallback CharEvent(char c) {
-	//if(m_lMode!=WORDS)
-	static int32_t flag_input = 0;
-	static int32_t num = 0;
-	static MsgQueue Msg;
-	int32_t length = 0;
-	int32_t length_Temp = num;
-	while (length_Temp != 0) {
-		length++;
-		length_Temp = length_Temp / 10;
+	//static String Words = NULL;
+	static int32_t String_Index = 0;
+	if (m_lMode != WORDS_MODE) {
+		static int32_t flag_input = 0;
+		static int32_t num = 0;
+		static MsgQueue Msg;
+		int32_t length = 0;
+		int32_t length_Temp = num;
+		while (length_Temp != 0) {
+			length++;
+			length_Temp = length_Temp / 10;
+		}
+		if (0 == flag_input) flag_input = 1;
+		if (CHAR_ENTER == c) {
+			flag_input++;
+			num = 0;
+		}
+		beginPaint();
+		paintText(0, 0, CLEAN_LINE);
+		switch (flag_input)
+		{
+		case 0:break;
+		case 1:
+			//New feature caretpos.
+			SetCaretPos(CARET_POSITON_X_SIZE + length*SIZE_PER_CARET, CARET_POSITON_Y);
+			showCaret();
+			if (isdigit(c)) {
+				num = num * 10 + c - '0';
+			}
+			String Dynamic_Size;
+			sprintf(Dynamic_Size, "size = %d pixels", num);
+			paintText(0, 0, Dynamic_Size);
+			CAD_Msg.Size = num;
+			break;
+		case 2:
+			SetCaretPos(CARET_POSITON_X_STYLE + length*SIZE_PER_CARET, CARET_POSITON_Y);
+			if (isdigit(c)) {
+				num = num * 10 + c - '0';
+			}
+			sprintf(Dynamic_Size, "style = %d", num);
+			paintText(0, 0, Dynamic_Size);
+			CAD_Msg.Style = num;
+			break;
+		case 3:
+			flag_input = 0;
+			hideCaret();
+			break;
+		default:
+			break;
+		}
+		endPaint();
 	}
-	if (0 == flag_input) flag_input = 1;
-	if (CHAR_ENTER == c) {
-		flag_input++;
-		num = 0;
-	}
-	beginPaint();
-	paintText(0, 0, CLEAN_LINE);
-	switch (flag_input)
-	{
-	case 0:break;
-	case 1:
-		//New feature
-		SetCaretPos(CARET_POSITON_X_SIZE+length*SIZE_PER_CARET,CARET_POSITON_Y );
+	else {
+		int32_t strlength = strlen(Words);
+		SetCaretPos(m_Text_Postion_x+ strlength*SIZE_PER_CARET, m_Text_Postion_y);
 		showCaret();
-		if (isdigit(c)) {
-			num = num * 10 + c - '0';
-		}
-		String Dynamic_Size;
-		sprintf(Dynamic_Size, "size = %d pixels", num);
-		paintText(0, 0, Dynamic_Size);
-		CAD_Msg.Size = num;
-		break;
-	case 2:
-		SetCaretPos(CARET_POSITON_X_STYLE + length*SIZE_PER_CARET, CARET_POSITON_Y);
-		if (isdigit(c)) {
-			num = num * 10 + c - '0';
-		}
-		sprintf(Dynamic_Size, "style = %d", num);
-		paintText(0, 0, Dynamic_Size);
-		CAD_Msg.Style = num;
-		break;
-	case 3:
-		flag_input = 0;
-		hideCaret();
-		break;
-	default:
-		break;
+		//paintText(0, 0, Dynamic_Size);
+		//CAD_Msg.Size = num;
 	}
-	endPaint();
 }
 KeyboardEventCallback KeyboardEvent(int key, int event) {
 	printf("Key = %d,event = %d\n", key, event);
@@ -136,6 +163,14 @@ MouseEventCallback MouseEvent(int x, int y, int button, int event) {
 		endPaint();
 		m_lMode = 3;
 	}
+	else if (x > CURVE_BUTTON_X_LEFT && x<CURVE_BUTTON_X_RIGHT && y>CURVE_BUTTON_Y_UP && y < CURVE_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 4;
+	}
 	else if (x > CRAMING_BUTTON_X_LEFT && x<CRAMING_BUTTON_X_RIGHT && y>CRAMING_BUTTON_Y_UP && y < CRAMING_BUTTON_Y_DOWN && button == 1 && event == 0) {
 		beginPaint();
 		setPenColor(BLACK);
@@ -144,7 +179,7 @@ MouseEventCallback MouseEvent(int x, int y, int button, int event) {
 		endPaint();
 		m_lMode = 5;
 	}
-	else if (x > LINE_BUTTON_X_LEFT && x< LINE_BUTTON_X_RIGHT && y > LINE_BUTTON_Y_UP && y < LINE_BUTTON_Y_DOWN&& button == 1 && event == 0) {
+	else if (x > LINE_BUTTON_X_LEFT && x< LINE_BUTTON_X_RIGHT && y > LINE_BUTTON_Y_UP && y < LINE_BUTTON_Y_DOWN && button == 1 && event == 0) {
 		beginPaint();
 		setPenColor(BLACK);
 		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
@@ -152,6 +187,125 @@ MouseEventCallback MouseEvent(int x, int y, int button, int event) {
 		endPaint();
 		m_lMode = 2;
 	}
+
+	else if (x > WORDS_BUTTON_X_LEFT && x< WORDS_BUTTON_X_RIGHT && y> WORDS_BUTTON_Y_UP && y < WORDS_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 7;
+	}
+	else if (x > CIRCLE_BUTTON_X_LEFT && x< CIRCLE_BUTTON_X_RIGHT && y> CIRCLE_BUTTON_Y_UP && y < CIRCLE_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 6;
+	}
+	else if (x >REGULAR_TRIANGLE_BUTTON_X_LEFT && x< REGULAR_TRIANGLE_BUTTON_X_RIGHT && y> REGULAR_TRIANGLE_BUTTON_Y_UP && y < REGULAR_TRIANGLE_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 9;
+	}
+	else if (x >ARC_RECTANGLE_BUTTON_X_LEFT && x< ARC_RECTANGLE_BUTTON_X_RIGHT && y> ARC_RECTANGLE_BUTTON_Y_UP && y < ARC_RECTANGLE_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 10;
+	}
+	else if (x >RIGHT_TRIANGLE_BUTTON_X_LEFT && x< RIGHT_TRIANGLE_BUTTON_X_RIGHT && y>RIGHT_TRIANGLE_BUTTON_Y_UP && y < RIGHT_TRIANGLE_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 11;
+	}
+	else if (x >FREE_SHAPE_BUTTON_X_LEFT && x< FREE_SHAPE_BUTTON_X_RIGHT && y>FREE_SHAPE_BUTTON_Y_UP && y <FREE_SHAPE_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 11;//12 in contribution
+	}
+	else if (x >FORE_STAR_BUTTON_X_LEFT && x< FORE_STAR_BUTTON_X_RIGHT && y>FORE_STAR_BUTTON_Y_UP && y < FORE_STAR_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 16;
+	}
+	else if (x >PENTAGON_BUTTON_X_LEFT && x< PENTAGON_BUTTON_X_RIGHT && y>PENTAGON_BUTTON_Y_UP && y < PENTAGON_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 14;
+	}
+	else if (x >HEXAGON_BUTTON_X_LEFT && x< HEXAGON_BUTTON_X_RIGHT && y>HEXAGON_BUTTON_Y_UP && y < HEXAGON_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 15;
+	}
+	else if (x >RHOMBUS_BUTTON_X_LEFT && x< RHOMBUS_BUTTON_X_RIGHT && y>RHOMBUS_BUTTON_Y_UP && y < RHOMBUS_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 13;
+	}
+	else if (x >FIVE_STAR_BUTTON_X_LEFT && x<FIVE_STAR_BUTTON_X_RIGHT && y>FIVE_STAR_BUTTON_Y_UP && y < FIVE_STAR_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 17;
+	}
+	else if (x >SIX_STAR_BUTTON_X_LEFT && x<SIX_STAR_BUTTON_X_RIGHT && y>SIX_STAR_BUTTON_Y_UP && y < SIX_STAR_BUTTON_Y_DOWN && button == 1 && event == 0) {
+		beginPaint();
+		setPenColor(BLACK);
+		paintText(0, 0, CLEAN_LINE);
+		paintText(0, 0, "Please input Size(pixels) and Style. Seperated by enter");
+		setPenColor(m_CurrentColor);
+		endPaint();
+		m_lMode = 18;
+	}
+	/*int32_t	Rgular_Triangle(int x0, int y0, int size, int style) { ; }
+int32_t Right_Triangle(int x0, int y0, int size, int style) { ; }
+int32_t Five_Star(int x0, int y0, int size, int style) { ; }
+int32_t Up_Arrow(int x0, int y0, int size, int style) { ; }
+int32_t DrawSquare(int x0, int y0, int size, int style) { ; }
+int32_t DrawCurve(int x0, int y0, int size, int style) { ; }
+int32_t DrawCircle(int x0, int y0, int size, int style) { ; }
+int32_t Arc_Rectangle(int x0, int y0, int size, int style) { ; }
+int32_t DrawPentagon(int x0, int y0, int size, int style) { ; }
+int32_t DownArrow(int x0, int y0, int size, int style) { ; }
+int32_t Hexagon(int x0, int y0, int size, int style) { ; }*/
 	if (0 != m_lMode && PressDown == 1)
 		//Beacuse we have varible PressDown we don't care the event and button.
 	{
@@ -166,10 +320,41 @@ MouseEventCallback MouseEvent(int x, int y, int button, int event) {
 			//Erase mode.
 			break;
 		case 5:
-			Craming(x,y, CAD_Msg.Size, CAD_Msg.Style);
+			Craming(x, y, CAD_Msg.Size, CAD_Msg.Style);
 			break;
 		case 2:
 			StraightLine(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 7:
+			Words(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 9:
+			Regular_Triangle(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 10:
+			Arc_Rectangle(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 11:
+			Right_Triangle(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 13:
+			Rhombus(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 14:
+			DrawPentagon(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 15:
+			Hexagon(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 16:
+			ForeStar(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 17:
+			Five_Star(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
+		case 18:
+			Six_Star(x, y, CAD_Msg.Size, CAD_Msg.Style);
+			break;
 		}
 	}
 }
@@ -221,7 +406,7 @@ int32_t Erase(int x0, int y0, int size, int style) {
 			setPenColor(m_CurrentColor);
 			endPaint();
 			break;
-		default :
+		default:
 			printf("In msgBox\n");
 			msgBox("Style out of range", "Please input style range from 1 to 2", Alert_Flag);
 			Alert_Flag = -1;
@@ -327,5 +512,9 @@ int32_t StraightLine(int x0, int y0, int size, int style) {
 	Pre_y = y0;
 }
 int32_t Words(int x0, int y0, int size, int style) {
-
+	beginPaint();
+	hideCaret();
+	endPaint();
+	m_Text_Postion_x = x0;
+	m_Text_Postion_y = y0;
 }
